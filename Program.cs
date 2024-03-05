4977e58a1e643f17fe3e1af60e636f1b5c31c7f0
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-03-05 19:56:39 +0100

    firstly, check if the system is > 9, then throw exception, if not, check if number contains any dots or commas, then search for its index

--- Program.cs
+++ Program.cs
@@ -4,11 +4,23 @@
     {
         static void Main(string[] args)
         {
-            
+            ToDecimal("123,123", 10);
         }
-        static double ToDecimal(string number, int system)
+        static double ToDecimal(string number, byte system)
         {
-
+            if(system > 9)
+            {
+                throw new ArgumentException("Wartość systemu liczbowego nie może być większa lub równa 10!");
+            }
+            if (number.Contains(".") || number.Contains(","))
+            {
+                int dotIndex = number.IndexOf('.');
+                if (dotIndex == -1)
+                {
+                    dotIndex = number.IndexOf(",");
+                }
+            }
+            return 0.0;
         }
     }
 }
